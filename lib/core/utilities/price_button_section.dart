34d4ae3c0e258a143f2b1023f23a1bbeb679bbf2
baselhegazi <baselmohammed226@gmail.com>
2025-10-17 00:24:12 +0300

    feat: implement PriceButtonSection widget for displaying product price and action button
    - create reusable PriceButtonSection with responsive layout
    - display title and value with styled CustomText widgets
    - support custom action widget (e.g., Add to Cart button)
    - add subtle shadow and background styling for separation from main content

---
+++ lib/core/utilities/price_button_section.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_e_commerce_app_2025/core/helper/const.dart';
+
+import '../../../../../core/utilities/custom_text.dart';
+
+class PriceButtonSection extends StatelessWidget {
+  const PriceButtonSection({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.widget,
+  });
+  final String title, value;
+  final Widget widget;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height * .1,
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 30),
+      decoration: BoxDecoration(
+        color: kScaffoldColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(16),
+            blurRadius: 6,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        spacing: 8,
+        children: [
+          Flexible(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: CustomText(
+                    text: title,
+                    fontSize: 14,
+                    alignment: Alignment.centerLeft,
+                    color: kGreyColor,
+                  ),
+                ),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: CustomText(
+                    text: value,
+                    fontSize: 18,
+                    alignment: Alignment.centerLeft,
+                    color: kPrimaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: widget),
+        ],
+      ),
+    );
+  }
+}
