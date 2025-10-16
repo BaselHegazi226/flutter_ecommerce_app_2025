99eada6beb6fc39d6c7ff854516c83decb7256bb
baselhegazi <baselmohammed226@gmail.com>
2025-10-17 00:21:29 +0300

    feat: add cart interaction and toast feedback in ProductDetailsPriceButtonSection - Implemented Add to Cart functionality using CartBloc events - Displayed toast notifications for success, failure, and duplicate item cases - Integrated PriceButtonSection and CustomButton for consistent design - Added debug logging for product model tracking during user action - Enhanced UX with real-time feedback using toastification service

---
+++ lib/features/04_home_view/presentation/view/widgets/product_details_price_button_section.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_e_commerce_app_2025/core/utilities/custom_button.dart';
+import 'package:flutter_e_commerce_app_2025/core/utilities/price_button_section.dart';
+import 'package:flutter_e_commerce_app_2025/core/utilities/toastnotification.dart';
+import 'package:flutter_e_commerce_app_2025/features/04_home_view/data/model/cart_model.dart';
+import 'package:flutter_e_commerce_app_2025/features/04_home_view/data/model/product_model.dart';
+import 'package:flutter_e_commerce_app_2025/features/05_cart_view/presentation/view_model/cart_bloc/cart_bloc.dart';
+import 'package:flutter_e_commerce_app_2025/features/05_cart_view/presentation/view_model/cart_bloc/cart_state.dart';
+import 'package:toastification/toastification.dart';
+
+class ProductDetailsPriceButtonSection extends StatelessWidget {
+  const ProductDetailsPriceButtonSection({
+    super.key,
+    required this.productModel,
+  });
+
+  final ProductModel productModel;
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('product model = ${productModel.toJson()}');
+    return PriceButtonSection(
+      title: 'Price',
+      value: '\$ ${productModel.price}',
+      widget: BlocConsumer<CartBloc, CartState>(
+        builder: (context, state) {
+          return CustomButton(
+            onPressed: () {
+              context.read<CartBloc>().add(
+                AddToCartEvent(
+                  cartModel: CartModel(productModel: productModel),
+                ),
+              );
+            },
+            text: 'Add To Cart',
+          );
+        },
+        listener: (context, state) {
+          if (state is AddToCartSuccess) {
+            ToastNotification.flatToastNotificationService(
+              onAutoCompleteCompleted: (value) {},
+              toastNotificationType: ToastificationType.success,
+            );
+          } else if (state is AddToCartFailure) {
+            ToastNotification.flatToastNotificationService(
+              onAutoCompleteCompleted: (value) {},
+              toastNotificationType: ToastificationType.error,
+            );
+            debugPrint('error errrrrrrrrrrrrror = ${state.errorMessage}');
+          } else if (state is AddToCartDone) {
+            ToastNotification.minimalToastNotificationService(
+              toastNotificationType: ToastificationType.info,
+              title: productModel.title.split(" ").take(2).join(" "),
+              description: 'is already added',
+
+              onAutoCompleteCompleted: (value) {},
+            );
+          } else {
+            print('loading add add adddddddd');
+          }
+        },
+      ),
+    );
+  }
+}
