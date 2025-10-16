c1939f2722bbc523b0672949d43905bd6c1ad037
baselhegazi <baselmohammed226@gmail.com>
2025-10-17 00:26:45 +0300

    feat: implement HomeView with MultiBlocProvider integration
    - provide CategoryCubit and ProductCubit using dependency injection
    - trigger initial category and best-selling product fetch on view load
    - wrap content inside SafeArea and Scaffold for consistent layout
    - set background color to match app theme
    - display HomeViewBody as main content

---
+++ lib/features/04_home_view/presentation/view/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_e_commerce_app_2025/core/helper/const.dart';
+import 'package:flutter_e_commerce_app_2025/core/utilities/app_get.dart';
+import 'package:flutter_e_commerce_app_2025/features/04_home_view/presentation/view/widgets/home_view_body.dart';
+import 'package:flutter_e_commerce_app_2025/features/04_home_view/presentation/view_model/category_cubit/category_cubit.dart';
+import 'package:flutter_e_commerce_app_2025/features/04_home_view/presentation/view_model/product_cubit/product_cubit.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              AppGet().getIt.get<CategoryCubit>()..getCategories(),
+        ),
+        BlocProvider(
+          create: (context) =>
+              AppGet().getIt.get<ProductCubit>()..getBestSellingProducts(),
+        ),
+      ],
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: kScaffoldColor,
+          body: const HomeViewBody(),
+        ),
+      ),
+    );
+  }
+}
