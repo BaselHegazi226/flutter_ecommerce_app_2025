12d9ad3e56a01f0755e6e77e3ee0afb15f2f94c9
baselhegazi <baselmohammed226@gmail.com>
2025-10-19 18:00:12 +0300

    update settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "com.example.flutter_e_commerce_app_2025"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -28,7 +28,7 @@ android {
         applicationId = "com.example.flutter_e_commerce_app_2025"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
