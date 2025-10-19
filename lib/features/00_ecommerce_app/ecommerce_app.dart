e7ac51d6673a795726fdb8c5c73516a4517270cc
baselhegazi <baselmohammed226@gmail.com>
2025-10-19 17:48:21 +0300

    refactor: rename import path for router configuration
    - update import from `app_router.dart` to `go_router.dart` for consistency with file naming convention
    - maintain same `AppRouter` usage within `EcommerceApp`
    - no functional changes, purely organizational for better clarity

--- lib/features/00_ecommerce_app/ecommerce_app.dart
+++ lib/features/00_ecommerce_app/ecommerce_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:toastification/toastification.dart';
 
-import '../../core/utilities/go_router.dart';
+import '../../core/utilities/app_router.dart';
 
 class EcommerceApp extends StatelessWidget {
   const EcommerceApp({super.key});
