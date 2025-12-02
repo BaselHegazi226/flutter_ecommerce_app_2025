84ef523735844a66db365d47eb7d82d1fd857aaf
baselhegazi <baselmohammed226@gmail.com>
2025-12-03 00:31:31 +0200

    add new routes to 1-searchView 2-showAllView

--- lib/core/helper/routes.dart
+++ lib/core/helper/routes.dart
@@ -6,6 +6,7 @@ class Routes {
   static const String categoryView = '/categoryView';
   static const String productDetailsView = '/productDetailsView';
   static const String homeView = '/homeView';
+  static const String searchView = '/searchView';
   static const String cartView = '/cartView';
   static const String checkoutView = '/checkoutView';
   static const String profileView = '/profileView';
@@ -14,4 +15,5 @@ class Routes {
   static const String orderHistoryView = '/orderHistoryView';
   static const String settingsView = '/settingsView';
   static const String favoriteView = '/favoriteView';
+  static const String showAllView = '/showAllView';
 }
