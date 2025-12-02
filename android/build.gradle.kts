d4929f883be48e9810e5c8c0a0151b788d8af715
baselhegazi <baselmohammed226@gmail.com>
2025-12-03 00:30:43 +0200

    updates settings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // ...
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     // Add the dependency for the Google services Gradle plugin
     id ("com.google.gms.google-services") version "4.4.3" apply false
