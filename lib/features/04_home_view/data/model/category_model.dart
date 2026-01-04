a28fd920509c3a9c4b956bc983feef51b543c13e
baselhegazi <baselmohammed226@gmail.com>
2026-01-04 06:38:12 +0200

    change of image parameter to requestForCategoryUrl

--- lib/features/04_home_view/data/model/category_model.dart
+++ lib/features/04_home_view/data/model/category_model.dart
@@ -1,21 +1,23 @@
 class CategoryModel {
   final String slug;
   final String name;
-  final String image;
+  final String requestForCategoryUrl;
+
   const CategoryModel({
     required this.slug,
     required this.name,
-    required this.image,
+    required this.requestForCategoryUrl,
   });
 
   factory CategoryModel.fromJson(json) {
     return CategoryModel(
       slug: json['slug'],
       name: json['name'],
-      image: json['url'],
+      requestForCategoryUrl: json['url'],
     );
   }
+
   toJson() {
-    return {'slug': slug, 'name': name, 'image': image};
+    return {'slug': slug, 'name': name, 'image': requestForCategoryUrl};
   }
 }
