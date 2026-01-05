93fe57a941a601d393f35c5ef2a557dafb6ab494
baselhegazi <baselmohammed226@gmail.com>
2026-01-05 05:24:19 +0200

    support tablet , mobile layout

--- lib/features/04_home_view/presentation/view/widgets/show_all_view_body.dart
+++ lib/features/04_home_view/presentation/view/widgets/show_all_view_body.dart
@@ -2,16 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_e_commerce_app_2025/core/helper/extensions_of_s_localization.dart';
 import 'package:flutter_e_commerce_app_2025/core/shimmer/custom_sliver_grid_shimmer.dart';
+import 'package:flutter_e_commerce_app_2025/core/utilities/custom_layout.dart';
 import 'package:flutter_e_commerce_app_2025/core/utilities/custom_sliver_grid_list_view.dart';
 import 'package:flutter_e_commerce_app_2025/features/04_home_view/presentation/view_model/show_all_cubit/show_all_cubit.dart';
 import 'package:flutter_e_commerce_app_2025/generated/l10n.dart';
-import 'package:go_router/go_router.dart';
 
-import '../../../../../core/helper/routes.dart';
 import '../../../../../core/utilities/custom_text.dart';
 
 class ShowAllViewBody extends StatelessWidget {
   const ShowAllViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomLayout(
+      mobileWidget: ShowAllViewBodyMobile(),
+      tabletWidget: ShowAllViewBodyTablet(),
+    );
+  }
+}
+
+class ShowAllViewBodyMobile extends StatelessWidget {
+  const ShowAllViewBodyMobile({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -32,40 +43,65 @@ class ShowAllViewBody extends StatelessWidget {
               ),
             );
           }
-          return GridView.builder(
-            itemCount: products.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, // خلي اتنين جنب بعض
-              crossAxisSpacing: 16, // مسافة أفقية بين العناصر
-              mainAxisSpacing: 12, // مسافة رأسية بين العناصر
-              childAspectRatio: 0.55, // النسبة بين العرض والارتفاع
-            ),
-            itemBuilder: (context, index) {
-              final product = products[index];
-              return CustomSliverGridItem(
-                screenSize: screenSize,
-                image: product.images[0],
-                title: product.title,
-                description: product.description,
-                price: product.price,
-                onTap: () {
-                  GoRouter.of(
-                    context,
-                  ).push(Routes.productDetailsView, extra: product.id);
-                },
-              );
-            },
-          );
+          return CustomGridListView(items: products);
         } else if (state is ShowAllFailure) {
           return Center(
             child: CustomText(
-              text: '${state.errorMessage} and we will repair it soon!',
+              text: state.errorMessage,
               fontSize: 18,
               alignment: Alignment.center,
             ),
           );
         }
         return customGridViewListShimmer(size: screenSize);
+      },
+    );
+  }
+}
+
+class ShowAllViewBodyTablet extends StatelessWidget {
+  const ShowAllViewBodyTablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
+    return BlocBuilder<ShowAllCubit, ShowAllState>(
+      builder: (context, state) {
+        if (state is ShowAllSuccess) {
+          final products = state.products;
+          if (products.isEmpty) {
+            return Center(
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                child: CustomText(
+                  text: S.of(context).homeNoProduct,
+                  fontSize: 20,
+                  alignment: Alignment.center,
+                ),
+              ),
+            );
+          }
+          return CustomGridListView(
+            items: products,
+            childAspectRatio: .65,
+            itemsInLine: 3,
+            crossAxisSpacing: 28,
+          );
+        } else if (state is ShowAllFailure) {
+          return Center(
+            child: CustomText(
+              text: state.errorMessage,
+              fontSize: 18,
+              alignment: Alignment.center,
+            ),
+          );
+        }
+        return customGridViewListShimmer(
+          size: screenSize,
+          childAspectRatio: .65,
+          itemsInLine: 3,
+          crossAxisSpacing: 28,
+        );
       },
     );
   }
